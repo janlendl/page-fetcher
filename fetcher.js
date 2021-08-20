610cd377a62833907600ca585294dbb11a6239da
Lendl Peralta <janlendl@gmail.com>
2021-08-20 15:30:35 +0000

    refactor variables and changed filePath and writeToFile to urlPath and fileToSave

--- fetcher.js
+++ fetcher.js
@@ -2,22 +2,22 @@ const request = require('request');
 const fs = require('fs');
 
 const requestURL = process.argv.slice(2);
-const filePath = requestURL[0];
-const writeToLocation = requestURL[1];
+const urlPath = requestURL[0];
+const fileToSave = requestURL[1];
 
 
-request(filePath, (error, response, body) => {
+request(urlPath, (error, response, body) => {
   if(error || response.statusCode !== 200) {
     console.log(`Error ${response.statusCode}, please check again!`);
     return;
   }
-  if (!writeToLocation.includes('.html')) {
-    console.log(`${writeToLocation} is an invalid file path. Please check again.`);
+  if (!fileToSave.includes('.html')) {
+    console.log(`${fileToSave} is an invalid file path. Please check again.`);
     return;
   }
-  fs.writeFile(writeToLocation, body, (error, stats) => {
+  fs.writeFile(fileToSave, body, (error, stats) => {
     if (!error) {
-      console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${writeToLocation}`);
+      console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${fileToSave}`);
     }
   }); 
 });
